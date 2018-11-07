82d4900bf8fdc89eeef0519803eb15e533692f7f
Bourgin Jeremy <jeremy.bourgin@etu.umontpellier.fr>
2018-11-07 14:15:58 +0100

    vue virtuelle

---
+++ mini_projet/implementation/virtualized_views.sql
@@ -0,0 +1,20 @@
+CREATE VIEW Date_travel
+AS SELECT * FROM Date_t;
+
+CREATE VIEW Date_maintenance
+AS SELECT * FROM Date_t;
+
+CREATE VIEW Time_travel
+AS SELECT * FROM Time_t;
+
+CREATE VIEW Time_maintenance
+AS SELECT * FROM Time_t;
+
+CREATE VIEW Time_maintenance
+AS SELECT * FROM Time_t;
+
+CREATE VIEW Vehicle_travel
+AS SELECT * FROM Vehicle;
+
+CREATE VIEW Vehicle_maintenance
+AS SELECT * FROM Vehicle;
