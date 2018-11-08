c65f5a5c88d05adb8f1531c6f12ee11026095f8a
Bourgin Jeremy <jeremy.bourgin@etu.umontpellier.fr>
2018-11-08 13:34:55 +0100

    insert travel

--- mini_projet/implementation/jeremy_select.sql
+++ mini_projet/implementation/jeremy_select.sql
@@ -1,26 +1,27 @@
-SELECT line.num_line, COUNT(traveler.id)
-FROM travel
-INNER JOIN line
-	ON travel.id_line = line.id
-INNER JOIN traveler
-	ON travel.id_traveler = traveler.id
-INNER JOIN date_t
-	ON travel.id_date = date_t.id
-WHERE date_t.year = YEAR(getdate())
-AND date_t.month_year >= (MONTH(getdate()) - 2)
-AND traveler.anonymous = 0
-GROUP BY line.num_line;
+SELECT Line.num_line, COUNT(Traveler.id) AS number_travelers
+FROM Travel
+INNER JOIN Line
+	ON Travel.id_line = Line.id
+INNER JOIN Traveler
+	ON Travel.id_traveler = Traveler.id
+INNER JOIN Date_t
+	ON Travel.id_date = Date_t.id
+WHERE Date_t.year = YEAR(getdate())
+AND Date_t.month_year >= (MONTH(getdate()) - 2)
+AND Traveler.anonymous = 0
+GROUP BY Line.num_line;
 
-SELECT line.num_line, MAX(COUNT(vehicle.id))
-FROM travel
-INNER JOIN line
-	ON travel.id_line = line.id
-INNER JOIN vehicle
-	ON vehicle.id_vehicle = vehicle.id
-GROUP BY line.num_line;
+SELECT Line.num_line, Vehicle.id, COUNT(Vehicle.id)
+FROM Travel
+INNER JOIN Line
+	ON Travel.id_line = Line.id
+INNER JOIN Vehicle
+	ON Travel.id_vehicle = Vehicle.id
+GROUP BY Line.num_line
+ORDER BY Line.num_line, Vehicle.id;
 
-SELECT COUNT(*)
-FROM maintenance
-INNER JOIN vehicle
-	ON vehicle.id_vehicle = vehicle.id
-WHERE vehicle.type = 'bus';
+SELECT COUNT(*) AS number_maintained_bus
+FROM Maintenance
+INNER JOIN Vehicle
+	ON Vehicle.id_vehicle = Vehicle.id
+WHERE Vehicle.type = 'bus';
